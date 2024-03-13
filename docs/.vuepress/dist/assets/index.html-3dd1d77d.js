d23a19573e7c3d116df11d33dbce5ed3f341a029
Rudi Erbe <rudi.erbe@softceed.de>
2024-03-13 12:57:11 +0100

    initial changes

---
+++ docs/.vuepress/dist/assets/index.html-3dd1d77d.js
@@ -0,0 +1,17 @@
+import{_ as e,p as a,q as n,a1 as t}from"./framework-5866ffd3.js";const i={},s=t(`<h1 id="all-songs" tabindex="-1"><a class="header-anchor" href="#all-songs" aria-hidden="true">#</a> All Songs</h1><p>All pages here contain songs (in Swahili) that I settle on and decide to learn their lyrics, periodically.</p><details class="custom-container details"><p><code>songs</code> here (the directory and contents) is used as an example of document directory that will be a collection in Decap CMS. The <code>index.md</code> files are within directories named in the format &quot;YYYY-MM-DD&quot;.</p><p>Update as needed.</p><div class="language-txt" data-ext="txt"><pre class="language-txt"><code>. (Root)
+├── docs
+│   ├── .vuepress
+│   │   │   ...
+│   │   └── config.ts
+│   ├── README.md
+│   └── songs
+│       ├── 2023-04-10
+│       │   └── index.md
+│       ├── 2023-04-17
+│       │   └── index.md
+│       │   ...
+│       └── index.md (Current file)
+│   ...
+├── README.md
+└── yarn.lock
+</code></pre></div></details><p>The pages have a standard format.</p><h2 id="format" tabindex="-1"><a class="header-anchor" href="#format" aria-hidden="true">#</a> Format</h2><ul><li>Title</li><li>Links <ul><li>Youtube</li><li>Spotify</li></ul></li><li>Lyrics (with translations if available)</li></ul><h2 id="popular-songs" tabindex="-1"><a class="header-anchor" href="#popular-songs" aria-hidden="true">#</a> Popular songs</h2><ul><li><a href="/songs/2023-04-10">Tuko tayari (Mwanga Band)</a></li></ul>`,8),l=[s];function o(d,r){return a(),n("div",null,l)}const h=e(i,[["render",o],["__file","index.html.vue"]]);export{h as default};
